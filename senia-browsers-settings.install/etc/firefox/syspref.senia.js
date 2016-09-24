c42e2e72ee0847a1380cfd531819127fa070f447
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2016-09-24 10:26:51 +0200

    Updated

--- senia-browsers-settings.install/etc/firefox/syspref.senia.js
+++ senia-browsers-settings.install/etc/firefox/syspref.senia.js
@@ -1,1 +1,3 @@
-lockpref("browser.startup.homepage", "data:text/plain,browser.startup.homepage=http://www.ieslasenia.es/www/");
+// Senia settings
+lockPref("browser.startup.homepage", "http://www.ieslasenia.es/www/");
+
